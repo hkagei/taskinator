ef25a8caebae5726f5d8d7be2594b7bfa3e59e07
hkagei <k_dawg_1022@hotmail.com>
2022-04-06 23:26:34 -0600

    tweaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -76,10 +77,50 @@ var createTaskEl = function (taskDataObj) {
 
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
-  
+
   console.log(taskActionsEl);
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+var taskButtonHandler = function(event) {
+  // get target element from event
+  var targetEl = event.target;
+
+  // edit button was clicked
+  if (targetEl.matches(".edit-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    editTask(taskId); 
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+    var editTask = function(taskId) {
+      console.log("editing task #" + taskId);
+    
+      // get task list item element
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+      
+      // get content from task name and type
+      var taskName = taskSelected.querySelector("h3.task-name").textContent;
+      console.log(taskName);
+      var taskType = taskSelected.querySelector("span.task-type").textContent;
+      console.log(taskType);
+
+      document.querySelector("input[name='task-name']").value = taskName;
+      document.querySelector("select[name='task-type']").value = taskType;
+    };
+  } 
+  // delete button was clicked
+  else if (targetEl.matches(".delete-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+  var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
