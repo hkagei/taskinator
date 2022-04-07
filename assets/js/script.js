567bed6805679ff82063ffdd226b8bc0ed4089f9
hkagei <k_dawg_1022@hotmail.com>
2022-04-06 22:32:38 -0600

    tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,47 @@ if (!taskNameInput || !taskTypeInput) {
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+
+  var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    // create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+    // create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+    
+    actionContainerEl.appendChild(deleteButtonEl);
+    
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (var i = 0; i < statusChoices.length; i++) {
+      // create option element
+      var statusOptionEl = document.createElement("option");
+      statusOptionEl.textContent = statusChoices[i];
+      statusOptionEl.setAttribute("value", statusChoices[i]);
+    
+      // append to select
+      statusSelectEl.appendChild(statusOptionEl);
+    }
+
+actionContainerEl.appendChild(statusSelectEl);
+    return actionContainerEl;
+};
+
+
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -33,6 +74,10 @@ var createTaskEl = function (taskDataObj) {
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
+  var taskActionsEl = createTaskActions(taskIdCounter);
+  listItemEl.appendChild(taskActionsEl);
+  
+  console.log(taskActionsEl);
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
